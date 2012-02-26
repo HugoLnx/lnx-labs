8bf03c641c5dfc6b8ae5a187e6b14f3f3da8b0fa
Hugo Roque <hugolnx@gmail.com>
2012-02-25 22:39:04 -0200

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var fs = require("fs");
+var jsont = require("./lib/json-template.js").jsontemplate;
+
+var server = express.createServer();
+server.configure(function(){
+  server.use(express.logger());
+  server.use(express.bodyParser());
+  server.use(express.static("./public"));
+  server.set('views',__dirname + '/views/');
+});
+
+function views(name) {
+  return fs.readFileSync(server.settings.views+name,"utf-8");
+}
+function experimentHtml(experiment) {
+  return {
+    body: views("/adapters/"+experiment+".body.jsont"),
+    head: views("/adapters/"+experiment+".head.jsont")
+  }
+}
+
+server.get("/", function(req,res){
+  var body = jsont.expand(views('index.jsont'),{experiments: [{name:'Estrelas',url:'estrelas/',image:'/images/bla.png'}]});
+  var layout = jsont.expand(views('layout.jsont'),{"body-extra": body});
+  res.send(layout);
+});
+
+server.get("/estrelas", function(req,res){
+  var html = experimentHtml('estrelas');
+  var body = jsont.expand(html.body,{path: '/experiments/estrelas/'});
+  var head = jsont.expand(html.head,{path: '/experiments/estrelas/'});
+  var layout = jsont.expand(views('layout.jsont'),{"body-extra": body,"head-extra":head});
+  res.send(layout);
+})
+
+var port = process.env.PORT || 3000;
+
+console.log("Listening on port " + port);
+
+server.listen(port);
