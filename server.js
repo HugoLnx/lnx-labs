aea966032140c5b82cfadc57bb71a35e97ccc30e
Hugo Roque <hugolnx@gmail.com>
2012-06-14 11:19:38 -0300

    refactoring 'database' use

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var fs = require("fs");
 var app = require("./lib/app.js");
+var db = require("./lib/database.js");
 
 app.start(function(server) {
   server.get("/", function(req,res){
     res.render('index', {
-      "experiments": db('experiments'),
+      "experiments": db.experiments(),
       "forlayout": {
         "styles-extra":['index.css'],
         "forkme": "hugolnx/lnx-labs"
@@ -13,7 +13,7 @@ app.start(function(server) {
   });
 
   server.get(/\/(estrelas|pingos)/, function(req,res) {
-    var experiment = findExperimentById(req.params[0]);
+    var experiment = db.experiment(req.params[0]);
     res.render("adapters/"+experiment.id, {
       "path": experiment.path,
       "forlayout": {
@@ -23,18 +23,3 @@ app.start(function(server) {
     })
   });
 });
-
-function db(name) {
-  var json_s = fs.readFileSync(__dirname+'/db/'+name+'.json',"utf-8");
-  return JSON.parse(json_s);
-}
-
-function findExperimentById(id) {
-  var experiments = db('experiments');
-  for(var i in experiments) {
-    var experiment = experiments[i];
-    if(experiment.id === id){
-      return experiment;
-    }
-  }
-}
