8e3da54b120ec8f4265e55e25a6347856f7418d0
Hugo Roque <hugolnx@gmail.com>
2012-06-14 11:02:02 -0300

    refactoring server configuration

---
+++ config/server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var hbs = require("hbs");
+
+var server = express.createServer();
+server.configure(function(){
+  server.use(express.logger());
+  server.use(express.bodyParser());
+  server.use(express.static("./public"));
+  server.register('.hbs.html', hbs);
+  server.register('.html', hbs);
+  server.set('view engine', 'hbs.html');
+  server.set('views', __dirname + '/../views/');
+});
+
+exports.server = server;
--- server.js
+++ server.js
@@ -1,32 +1,5 @@
-var express = require("express");
 var fs = require("fs");
-var hbs = require("hbs");
-
-var server = express.createServer();
-server.configure(function(){
-  server.use(express.logger());
-  server.use(express.bodyParser());
-  server.use(express.static("./public"));
-  server.register('.hbs.html',hbs);
-  server.register('.html',hbs);
-  server.set('view engine', 'hbs.html');
-  server.set('views',__dirname + '/views/');
-});
-
-function db(name) {
-  var json_s = fs.readFileSync(__dirname+'/db/'+name+'.json',"utf-8");
-  return JSON.parse(json_s);
-}
-
-function findExperimentById(id) {
-  var experiments = db('experiments');
-  for(var i in experiments) {
-    var experiment = experiments[i];
-    if(experiment.id === id){
-      return experiment;
-    }
-  }
-}
+var server = require("./config/server.js").server;
 
 server.get("/", function(req,res){
   res.render('index', {
@@ -48,6 +21,21 @@ server.get(/\/(estrelas|pingos)/, function(req,res) {
     }
   })
 });
+
+function db(name) {
+  var json_s = fs.readFileSync(__dirname+'/db/'+name+'.json',"utf-8");
+  return JSON.parse(json_s);
+}
+
+function findExperimentById(id) {
+  var experiments = db('experiments');
+  for(var i in experiments) {
+    var experiment = experiments[i];
+    if(experiment.id === id){
+      return experiment;
+    }
+  }
+}
 
 var port = process.env.PORT || 3000;
 
